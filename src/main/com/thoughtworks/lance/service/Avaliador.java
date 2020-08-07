b3b57aa3858b7f7aa7bdc100ccbeb4302f675b3b
nicoledbianchin <nicoledbianchin@gmail.com>
2020-08-07 13:34:55 -0300

    adiciona mais um metodo

--- src/main/com/thoughtworks/lance/service/Avaliador.java
+++ src/main/com/thoughtworks/lance/service/Avaliador.java
@@ -3,10 +3,13 @@ package main.com.thoughtworks.lance.service;
 import main.com.thoughtworks.lance.domain.Leilao;
 import main.com.thoughtworks.lance.domain.Lance;
 
+import java.util.List;
+
 public class Avaliador {
 
     private double maiorDeTodos = Double.NEGATIVE_INFINITY;
     private double menorDeTodos = Double.POSITIVE_INFINITY;
+    private double media = 0;
 
     public void avalia(Leilao leilao) {
         for(Lance lance : leilao.getLances()) {
@@ -15,11 +18,26 @@ public class Avaliador {
         }
     }
 
+    public double calculaMedia(List<Double> valores) {
+        double soma = 0;
+        int quantidade = 0;
+        for (double valor : valores) {
+            soma = soma + valor;
+            quantidade++;
+        }
+        media = soma / quantidade;
+        return media;
+    }
+
     public double getMaiorLance() {
         return maiorDeTodos;
     }
 
     public double getMenorLance() {
         return menorDeTodos;
+    }
+
+    public double getMedia() {
+        return media;
     }
 }
--- src/test/service/AvaliadorTeste.java
+++ src/test/service/AvaliadorTeste.java
@@ -8,6 +8,11 @@ import main.com.thoughtworks.lance.service.Avaliador;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class AvaliadorTeste {
 
     @Test
@@ -28,7 +33,37 @@ public class AvaliadorTeste {
         double maiorEsperado = 400;
         double menorEsperado = 250;
 
+
         Assert.assertEquals(maiorEsperado, avaliador.getMaiorLance(), 0.000001);
         Assert.assertEquals(menorEsperado, avaliador.getMenorLance(), 0.000001);
+    }
+
+    @Test
+    public void deveCalcularMedia() {
+        Usuario trixie = new Usuario("Trixie");
+        Usuario katya = new Usuario("Katya");
+        Usuario jinkx = new Usuario("Jinkx");
+
+        Leilao leilao = new Leilao("Playstation 3");
+
+        Lance lanceTrixie = new Lance(trixie, 250.0);
+        leilao.propoe(lanceTrixie);
+        Lance lanceKatya = new Lance(katya, 300.0);
+        leilao.propoe(lanceKatya);
+        Lance lanceJinkx = new Lance(jinkx, 400.0);
+        leilao.propoe(lanceJinkx);
+
+        List<Double> lances = new ArrayList<>();
+        lances.add(lanceTrixie.getValor());
+        lances.add(lanceKatya.getValor());
+        lances.add(lanceJinkx.getValor());
+
+        Avaliador avaliador = new Avaliador();
+        avaliador.calculaMedia(lances);
+        double mediaAtual = avaliador.getMedia();
+
+        double mediaEsperada = 316.666666666666;
+
+        Assert.assertEquals(mediaEsperada, mediaAtual, 0.00001);
     }
 }
