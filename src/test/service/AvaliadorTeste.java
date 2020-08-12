06a54cdd5354f05e5f0d819c5405a7f715c24a6e
nicoledbianchin <nicoledbianchin@gmail.com>
2020-08-12 09:06:54 -0300

    :white_check_mark:

--- src/test/service/AvaliadorTeste.java
+++ src/test/service/AvaliadorTeste.java
@@ -147,4 +147,62 @@ public class AvaliadorTeste {
         assertEquals(400, avaliador.getMaiorLance(), 0.00001);
         assertEquals(100, avaliador.getMenorLance(), 0.00001);
     }
+
+    @Test
+    public void deveEncontrarTresMaioresEmCincoLances() {
+        Usuario trixie = new Usuario("Trixie");
+        Usuario katya = new Usuario("Katya");
+
+        Leilao leilao = new Leilao("Playstation 3");
+
+        leilao.propoe(new Lance(trixie, 400.0));
+        leilao.propoe(new Lance(katya, 300.0));
+        leilao.propoe(new Lance(trixie, 200.0));
+        leilao.propoe(new Lance(katya, 100.0));
+        leilao.propoe(new Lance(trixie, 700.0));
+
+        Avaliador avaliador = new Avaliador();
+        avaliador.avalia(leilao);
+
+        List<Lance> maiores = avaliador.getTresMaiores();
+
+        assertEquals(3, maiores.size());
+        assertEquals(700, maiores.get(0).getValor(), 0.00001);
+        assertEquals(400, maiores.get(1).getValor(), 0.00001);
+        assertEquals(300, maiores.get(2).getValor(), 0.00001);
+    }
+
+    @Test
+    public void deveEncontrarDoisMaioresEmDoisLances() {
+        Usuario trixie = new Usuario("Trixie");
+        Usuario katya = new Usuario("Katya");
+
+        Leilao leilao = new Leilao("Playstation 3");
+
+        leilao.propoe(new Lance(trixie, 400.0));
+        leilao.propoe(new Lance(katya, 300.0));
+
+        Avaliador avaliador = new Avaliador();
+        avaliador.avalia(leilao);
+
+        List<Lance> maiores = avaliador.getTresMaiores();
+
+        assertEquals(2, maiores.size());
+        assertEquals(400, maiores.get(0).getValor(), 0.00001);
+        assertEquals(300, maiores.get(1).getValor(), 0.00001);
+    }
+
+    @Test
+    public void deveEncontrarlistaVazia() {
+        Usuario trixie = new Usuario("Trixie");
+
+        Leilao leilao = new Leilao("Playstation 3");
+
+        Avaliador avaliador = new Avaliador();
+        avaliador.avalia(leilao);
+
+        List<Lance> maiores = avaliador.getTresMaiores();
+
+        assertEquals(0, maiores.size());
+    }
 }
