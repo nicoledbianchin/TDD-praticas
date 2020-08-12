8f005f13eda3dd908fa0572ccf786a543bd30bf5
nicoledbianchin <nicoledbianchin@gmail.com>
2020-08-12 09:31:54 -0300

    :white_check_mark: - calculadora

---
+++ src/test/matematica/MatematicaTest.java
@@ -0,0 +1,56 @@
+package test.matematica;
+
+import main.com.thoughtworks.matematica.Calculadora;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+
+public class MatematicaTest {
+
+    @Test
+    public void deveRetornarQuadruploComValorMaiorQueTrinta() {
+        Calculadora calculadora = new Calculadora();
+
+        int resultado = calculadora.conta(31);
+
+        Assert.assertThat(resultado, is(equalTo(124)));
+    }
+
+    @Test
+    public void deveRetornarTriploComValorMaiorQueDez() {
+        Calculadora calculadora = new Calculadora();
+
+        int resultado = calculadora.conta(11);
+
+        Assert.assertThat(resultado, is(equalTo(33)));
+    }
+
+    @Test
+    public void deveRetornarTriploComValorTrinta() {
+        Calculadora calculadora = new Calculadora();
+
+        int resultado = calculadora.conta(30);
+
+        Assert.assertThat(resultado, is(equalTo(90)));
+    }
+
+    @Test
+    public void deveRetornarDuploComValorMenorQueDez() {
+        Calculadora calculadora = new Calculadora();
+
+        int resultado = calculadora.conta(8);
+
+        Assert.assertThat(resultado, is(equalTo(16)));
+    }
+
+    @Test
+    public void deveRetornarDuploComValorDez() {
+        Calculadora calculadora = new Calculadora();
+
+        int resultado = calculadora.conta(10);
+
+        Assert.assertThat(resultado, is(equalTo(20)));
+    }
+}
