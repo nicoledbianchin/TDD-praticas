1773bfdeb26eba4643873d15a06b5518a0aa3769
nicoledbianchin <nicoledbianchin@gmail.com>
2020-08-27 16:34:30 -0300

    :sparkles: verificar se ano é bissexto

---
+++ src/anoBissexto/main/AnoBissexto.java
@@ -0,0 +1,9 @@
+package anoBissexto.main;
+
+public class AnoBissexto {
+    public boolean eBissexto(int ano) {
+        if (((ano % 4) == 0) && ((ano % 100) != 0)) return true;
+        else if ((ano % 400) == 0) return true;
+        else return false;
+    }
+}
